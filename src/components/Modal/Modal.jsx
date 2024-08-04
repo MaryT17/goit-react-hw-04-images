60d32a7d79ff65ff7e590477ae1e444d6ee9d494
MaryT17 <tmary11996@gmail.com>
2024-08-05 00:43:26 +0800

    update1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,44 @@
-
-// src/components/Modal/Modal.js
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import styles from './Modal.module.css'; // Make sure to create a corresponding CSS module
+import styles from './Modal.module.css';
 
-class Modal extends Component {
-  static propTypes = {
-    image: PropTypes.string.isRequired,
-    tags: PropTypes.string,
-    onClose: PropTypes.func.isRequired,
-  };
+const Modal = ({ image, tags, onClose }) => {
+  // Assign a reference to the onClose prop to stabilize its value
+  const onCloseRef = useRef(onClose);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  // Update the ref to the latest onClose function on every render
+  useEffect(() => {
+    onCloseRef.current = onClose;
+  }, [onClose]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    // Define handleKeyDown without having to add it in the dependency array
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onCloseRef.current();
+      }
+    };
 
-  handleKeyDown = e => {
-    console.log(e);
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []); // Empty dependency array
 
-  render() {
-    const { image, tags } = this.props;
-    return (
-      <div className={styles.overlay}>
-        <div className={styles.modal}>
-          <img src={image} alt={tags} />
-        </div>
+  // Render the modal content
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <img src={image} alt={tags} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  image: PropTypes.string.isRequired,
+  tags: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
 
 export default Modal;
